3e34da881c0d89afaba0f7fddac0e25d10aa79c9
Julio Paz <juliopaz1990@gmail.com>
2024-05-19 17:36:52 -0500

    cambio de cabecera v2.1

--- src/main/java/controlador/ServletControlador.java
+++ src/main/java/controlador/ServletControlador.java
@@ -64,7 +64,7 @@ public class ServletControlador extends HttpServlet {
                 request.setAttribute("usuario", usu);
 
                 
-                List<Usuario> usuarios = iusu.listar(); //prueba con rama v2
+                List<Usuario> usuarios = iusu.listar(); //prueba con rama v2.1
                 request.setAttribute("usuarios", usuarios);
 
                 request.getRequestDispatcher("principal.jsp").forward(request, response);
