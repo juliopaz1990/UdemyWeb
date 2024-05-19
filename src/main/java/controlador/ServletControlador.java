73f5a874cdab89d6ceeb6541eca19388ba3023f4
Julio Paz <juliopaz1990@gmail.com>
2024-05-18 19:06:42 -0500

    nuevo cambio 3

--- src/main/java/controlador/ServletControlador.java
+++ src/main/java/controlador/ServletControlador.java
@@ -64,7 +64,7 @@ public class ServletControlador extends HttpServlet {
                 request.setAttribute("usuario", usu);
 
                 
-                List<Usuario> usuarios = iusu.listar(); //prueba 2
+                List<Usuario> usuarios = iusu.listar(); //prueba 3
                 request.setAttribute("usuarios", usuarios);
 
                 request.getRequestDispatcher("principal.jsp").forward(request, response);
